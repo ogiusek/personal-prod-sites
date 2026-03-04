159b4df581d82ffd05e0599b2f2dc2538d4a1528
ogiusek <kowalewski.olgierd@gmail.com>
2026-03-04 16:39:08 +0100

    modified jenkins

--- ci-cd/jenkins/scripts/pull.sh
+++ ci-cd/jenkins/scripts/pull.sh
@@ -1,3 +1,10 @@
-git config --global --add safe.directory /personal-prod-sites
-git checkout ${GIT_BRANCH}
-git pull
+if [ -d ".git" ]; then
+  echo "Repository exists. Pulling latest changes..."
+  git fetch origin ${GIT_BRANCH}
+  git checkout ${GIT_BRANCH}
+  git pull origin ${GIT_BRANCH}
+else
+  echo "Repository does not exist. Cloning..."
+  git clone https://${GITHUB_APP}:${GITHUB_ACCESS_TOKEN}@github.com/ogiusek/.git .
+  git checkout ${GIT_BRANCH}
+fi
