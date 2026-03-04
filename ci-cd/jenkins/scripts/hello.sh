1dec0f401986e1c65d2b9dc8d515b2943ebfa62f
ogiusek <kowalewski.olgierd@gmail.com>
2026-03-04 16:51:59 +0100

    modified jenkins

--- ci-cd/jenkins/scripts/hello.sh
+++ ci-cd/jenkins/scripts/hello.sh
@@ -1,13 +1,2 @@
-# if [ -d ".git" ]; then
-#   echo "Repository exists. Pulling latest changes..."
-#   git fetch origin ${GIT_BRANCH}
-#   git checkout ${GIT_BRANCH}
-#   git pull origin ${GIT_BRANCH}
-# else
-#   echo "Repository does not exist. Cloning..."
-#   git clone https://${GITHUB_APP}:${GITHUB_ACCESS_TOKEN}@github.com/ogiusek/.git .
-#   git checkout ${GIT_BRANCH}
-# fi
-#
 echo "Hello World Scripts!"
 echo "Current branch from script is: ${GIT_BRANCH}"
