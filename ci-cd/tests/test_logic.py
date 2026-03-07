f13e4ca5ea1eaf1d444a9eb44af74df0f41cb4fd
ogiusek <kowalewski.olgierd@gmail.com>
2026-03-07 01:59:21 +0100

    modified tests

--- ci-cd/tests/test_logic.py
+++ ci-cd/tests/test_logic.py
@@ -8,6 +8,7 @@ class TestLogic(unittest.TestCase):
         self.assertEqual(add(10, 5), 15)
         self.assertEqual(add(-1, 1), 0)
         self.assertEqual(add(0, 0), 0)
+        self.assertEqual(add(1, 1), 2)
 
 
 if __name__ == "__main__":
